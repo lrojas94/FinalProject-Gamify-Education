0d4f8b98602fd35c74f8380cde099cdcb80e6703
Manuel E. Ureña <manuel.ureh@hotmail.com>
2016-06-18 16:02:05 -0400

    Add authentication with Auth0
    Done:
    
    * Added dependencies to use jwt with TypeScript
    * Modified Server side App file to check the jwt
    * Modified constants to add Auth0 api keys
    * Added cdn on index.html to import auth0
    
    To Do:
    
    * Implement Auth0 authentication on client side

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,26 @@
 /// <reference path="../typings/index.d.ts"/>
 import * as express from 'express';
+import * as jwt from 'express-jwt';
 import * as serveStatic from'serve-static';
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import * as db from './models/db';
+import {constants} from './constants';
+import {Authentication} from './models/Authentication';
 
 var app = express();
 var port = process.env.PORT || 3000;
+var jwtCheck = jwt({
+    secret: new Buffer(constants.API_BACKEND_CLIENT_SECRET, 'base64'),
+    audience: constants.API_BACKEND_CLIENT_ID
+});
 
 app.use(express.static('client/'));
 app.use(express.static('bower_components/'));
 app.use(serveStatic(path.join(__dirname, 'client/'), {'index': ['index.html']}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/api/login', jwtCheck);
 
 
 app.get('/api', function(req, res){
--- server/constants.ts
+++ server/constants.ts
@@ -2,5 +2,7 @@ export var constants = {
   DATABASE_NAME: 'gamifyeducation',
   DATABASE_USERNAME: 'gamifyeducation',
   DATABASE_PASSWORD: 'gamifyeducation',
-  DATABASE_HOST: 'localhost'
+  DATABASE_HOST: 'localhost',
+  API_BACKEND_CLIENT_SECRET: 'mTXVIlO4EOr7Bujpeo-6RaKFaRaPIaxwtsTACD8_XRvgiqwN8ADM0xVhas0oibWy',
+  API_BACKEND_CLIENT_ID: 'Db692KvERgLnSx5ilO7cYrFPPLNzEhYF'
 };
