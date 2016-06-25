3e8849ee9883caa70483aeda73ca47ffe54a9c25
=Frankie Garabito <=frankiegarabito@gmail.com>
2016-06-25 17:38:46 -0400

    Adding new school table tests

---
+++ server/tests/db/school.ts
@@ -0,0 +1,46 @@
+import * as db from '../../models/db';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import lang from '../../messages';
+
+chai.use(chaiAsPromised);
+chai.should();
+db.initialize();
+
+
+describe('School Model', () => {
+  beforeEach(() => {
+    return db.School.sync({
+      force: true
+    });
+  });
+  describe('Create school with no errors.', () => {
+    it('Should correctly create a person when information provided is valid and correct.', () => {
+      return db.School.create({
+        name: 'Juan XXIII',
+        telephone: '809-707-0266'
+      })
+      .then((person)=> {
+        school.name.should.equal('Juan XXIII');
+        school.telephone.should.equal('809-707-0266');
+      });
+    });
+  });
+
+  describe('School cannot have null values.', () => {
+    it(`Should throw error when having name = null.`,
+        () => {
+          return db.School.create({
+            name: null,
+            telephone: '809-707-0266'
+          }).should.eventually.be.rejected;
+      });
+
+      it(`Should throw error when having telephone = null.`,
+          () => {
+            return db.Person.create({
+              name: 'Juan XXIII',
+              telephone: null
+            }).should.eventually.be.rejected;
+      });
+    });
